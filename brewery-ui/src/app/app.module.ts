d18414e2f670fdc559de77291239b1709d66d7b3
LeonCarraro <lcarraro.oliveira@gmail.com>
2020-11-26 22:42:38 -0300

    Initial impl of beer search component

--- brewery-ui/src/app/app.module.ts
+++ brewery-ui/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BeerSearchComponent } from './components/beer-search/beer-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    BeerSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- brewery-ui/src/app/components/topbar/topbar.component.ts
+++ brewery-ui/src/app/components/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
@@ -7,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
+  screenWidth: number;
   openMenu = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getScreenWidth();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.getScreenWidth();
+  }
+
+  getScreenWidth() {
+    this.screenWidth = window.innerWidth;
   }
 
 }
